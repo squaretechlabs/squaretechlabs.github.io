15442ac89bf69855176531edd5a9e43c9c2b7585
ambarmishra <ambar.mishra93@gmail.com>
2021-02-03 01:51:16 +0530

    Mail feature.
    Google Excel Scripts Implemented replacing php mail().

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,42 @@
 <?php
-$to = 'ambar.mishra93@gmail.com';
-$subject = 'Marriage Proposal';
-$message = 'Hi Jane, will you marry me?'; 
-$from = 'ambar.mishra93@gmail.com';
- 
-// Sending email
-if(mail($to, $subject, $message)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/vendor/phpmailer/src/Exception.php';
+require_once __DIR__ . '/vendor/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/vendor/phpmailer/src/SMTP.php';
+
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'ambar.mishra93@gmail.com'; // YOUR gmail email
+    $mail->Password = '**************'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('yPandya@gmail.com', 'Sender Name');
+    $mail->addAddress('ypandya29.yp@gmail.com', 'Receiver Name');
+    $mail->addReplyTo('example@gmail.com', 'Sender Name'); // to set the reply to
+
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Send email using Gmail SMTP and PHPMailer";
+    $mail->Body = 'HTML message body. <b>Gmail</b> SMTP email body.';
+    $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+
+    $mail->send();
+    echo "Email message sent.";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
 }
+
 ?>
