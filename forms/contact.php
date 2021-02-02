ad08bf15cf18473a6faedce23193383c2046c8a9
ambarmishra <ambar.mishra93@gmail.com>
2021-02-03 00:36:03 +0530

    php mail change.

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,13 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("ambar.mishra93@gmail.com","My subject",$msg);
+$to = 'ambar.mishra93@gmail.com';
+$subject = 'Marriage Proposal';
+$message = 'Hi Jane, will you marry me?'; 
+$from = 'ambar.mishra93@gmail.com';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 ?>
